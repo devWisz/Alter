0eb6635345431f72cf355e329b3bb97bc360c5b1
USER WisZy <pythondevforuser@gmail.com>
2026-05-14 14:17:39 +0545

    it is working now

--- Alter/src/main.rs
+++
@@ -1,5 +0,0 @@
-extern crate flate2;
-
-use flate2::write ::GzEncoder;
-use flate2::Compression;
-use std::env::args;
--- Alter/target/debug/deps/cfg_if-1b00309adfde8577.d
+++
@@ -1,5 +0,0 @@
-C:\Users\sanke\Desktop\Alter\alter\target\debug\deps\cfg_if-1b00309adfde8577.d: C:\Users\sanke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\sanke\Desktop\Alter\alter\target\debug\deps\libcfg_if-1b00309adfde8577.rmeta: C:\Users\sanke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\sanke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
