483dba00ff65b33c42a933187f4e4c24a9018d11
USER WisZy <pythondevforuser@gmail.com>
2026-05-15 23:21:57 +0545

    organized code , add y/n functionality and other more changes

--- alterr/src/main.rs
+++ alterr/src/main.rs
@@ -1,12 +1,10 @@
-
 extern crate flate2;
 
-use flate2::write::GzEncoder;
 use flate2::Compression;
+use flate2::write::GzEncoder;
 use std::env::args;
 use std::fs::File;
-use std::io::copy;
-use std::io::BufReader;
+use std::io::{self, copy, BufReader, Write};
 use std::time::Instant;
 
 fn main() {
@@ -14,18 +12,35 @@ fn main() {
         eprintln!("Usage: `source` `target`");
         return;
     }
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output = File::create(args().nth(2).unwrap()).unwrap();
+
+    let source_path = args().nth(1).unwrap();
+    let target_path = args().nth(2).unwrap();
+
+    let mut input = BufReader::new(File::open(&source_path).unwrap());
+    let output = File::create(&target_path).unwrap();
+    
     let mut encoder = GzEncoder::new(output, Compression::default());
     let start = Instant::now();
+
     copy(&mut input, &mut encoder).unwrap();
-    let output = encoder.finish().unwrap();
-    println!(
-        "Source len: {:?}",
-        input.get_ref().metadata().unwrap().len()
-    );
+    let output_file = encoder.finish().unwrap();
+    let duration = start.elapsed();
+
+    let input_len = input.get_ref().metadata().unwrap().len();
+    let output_len = output_file.metadata().unwrap().len();
+
+    println!("Source len: {:?}", input_len);
+    println!("Target len: {:?}", output_len);
+    println!("Elapsed:    {:?}", duration);
+
+print!("\n Do you want to  compress one more file?(y/n):");
+io::stdout().flush().unwrap();
+
+let mut response = String::new();
+io::stdin().read_line(&mut response).unwrap();
 
 
-    println!("Target len: {:?}", output.metadata().unwrap().len());
-    println!("Elapsed: {:?}", start.elapsed());
-} 
+if response.trim().to_lowercase()!= "y"{
+    return;
+}
+}
---
+++ alterr/target/debug/deps/adler2-812557bf3732267a.d
@@ -0,0 +1,8 @@
+/mnt/c/Users/sanke/Desktop/Alter/alterr/target/debug/deps/adler2-812557bf3732267a.d: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/mnt/c/Users/sanke/Desktop/Alter/alterr/target/debug/deps/libadler2-812557bf3732267a.rlib: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/mnt/c/Users/sanke/Desktop/Alter/alterr/target/debug/deps/libadler2-812557bf3732267a.rmeta: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs:
+/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs:
